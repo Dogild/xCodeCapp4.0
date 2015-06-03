9fff2436d1d826ebabd81f2b16f0ae35d6811098
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-06-03 13:07:54 -0700

    cleanup AppController

--- XcodeCapp/AppDelegate.m
+++ XcodeCapp/AppDelegate.m
@@ -12,15 +12,103 @@
 
 @implementation AppDelegate
 
+
+#pragma mark - Utilities
+
+- (void)_initUserDefaults
+{
+    NSDictionary *appDefaults = @{
+                                kDefaultXCCAutoOpenXcodeProject: @YES,
+                                kDefaultXCCLogLevel: [NSNumber numberWithInt:LOG_LEVEL_WARN],
+                                kDefaultXCCMaxNumberOfOperations: @20
+                                };
+    
+    [[NSUserDefaults standardUserDefaults] registerDefaults:appDefaults];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (void)_initStatusItem
+{
+    self->imageStatusInactive   = [NSImage imageNamed:@"status-icon-inactive"];
+    self->imageStatusProcessing    = [NSImage imageNamed:@"status-icon-working"];
+    self->imageStatusError      = [NSImage imageNamed:@"status-icon-error"];
+    
+    self->statusItem                 = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    self->statusItem.menu            = self->statusMenu;
+    self->statusItem.image           = self->imageStatusInactive;
+    self->statusItem.highlightMode   = YES;
+    self->statusItem.length          = self->imageStatusInactive.size.width + 12;
+}
+
+- (void)_initLogging
+{
+#if DEBUG
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    [DDLogLevel setLogLevel:LOG_LEVEL_VERBOSE];
+#else
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    int logLevel = (int)[defaults integerForKey:kDefaultXCCLogLevel];
+    NSUInteger modifiers = [NSEvent modifierFlags];
+    
+    if (modifiers & NSAlternateKeyMask)
+        logLevel = LOG_LEVEL_VERBOSE;
+    
+    [DDLogLevel setLogLevel:logLevel];
+#endif
+}
+
+- (void)_initObservers
+{
+    [self.mainOperationQueue addObserver:self forKeyPath:@"operationCount" options:0 context:nil];
+    [self.mainWindowController addObserver:self forKeyPath:@"totalNumberOfErrors" options:0 context:nil];
+}
+
+
+#pragma mark - Actions
+
+- (IBAction)openPreferences:(id)aSender
+{
+    [self->preferencesWindow makeKeyAndOrderFront:nil];
+}
+
+- (IBAction)openAbout:(id)aSender
+{
+    [self->aboutWindow makeKeyAndOrderFront:nil];
+}
+
+
+#pragma mark - Observers
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    NSImage *image;
+    
+    if (self.mainOperationQueue.operationCount)
+        image = self->imageStatusProcessing;
+    else if ([self.mainWindowController totalNumberOfErrors])
+        image = self->imageStatusError;
+    else
+        image = self->imageStatusInactive;
+    
+    [self->statusItem performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:NO];
+}
+
+
+#pragma mark - Delegates
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     DDLogVerbose(@"\n******************************\n**    XcodeCapp started     **\n******************************\n");
     
     self.mainOperationQueue = [NSOperationQueue new];
     
-    [self registerDefaultPreferences];
-    [self initLogging];
+    [self _initLogging];
+    [self _initUserDefaults];
     [self _initStatusItem];
+    [self _initObservers];
     
     [self->_mainWindowController windowDidLoad];
 }
@@ -61,103 +149,4 @@
         return NO;
     }
 }
-
-- (void)_initStatusItem
-{
-    self->imageStatusInactive   = [NSImage imageNamed:@"status-icon-inactive"];
-    self->imageStatusProcessing    = [NSImage imageNamed:@"status-icon-working"];
-    self->imageStatusError      = [NSImage imageNamed:@"status-icon-error"];
-    
-    self->statusItem                 = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    self->statusItem.menu            = self->statusMenu;
-    self->statusItem.image           = self->imageStatusInactive;
-    self->statusItem.highlightMode   = YES;
-    self->statusItem.length          = self->imageStatusInactive.size.width + 12;
-    
-    [self.mainOperationQueue addObserver:self forKeyPath:@"operationCount" options:0 context:nil];
-    [self.mainWindowController addObserver:self forKeyPath:@"totalNumberOfErrors" options:0 context:nil];
-}
-
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
-{
-    NSImage *image;
-    
-    if (self.mainOperationQueue.operationCount)
-        image = self->imageStatusProcessing;
-    else if ([self.mainWindowController totalNumberOfErrors])
-        image = self->imageStatusError;
-    else
-        image = self->imageStatusInactive;
-    
-    [self->statusItem performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:NO];
-}
-
-#pragma mark - Window managements
-
-- (void)openWindow:(NSWindow *)aWindow
-{
-    [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
-    [aWindow makeKeyAndOrderFront:nil];
-}
-
-
-#pragma matk - Preferences window management
-
-- (IBAction)openPreferences:(id)aSender
-{
-    [self openWindow:self->preferencesWindow];
-}
-
-
-#pragma mark - About window management
-
-- (IBAction)openAbout:(id)aSender
-{
-    [self openWindow:self->aboutWindow];
-}
-
-- (NSString *)bundleVersion
-{
-    return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
-}
-
-
-#pragma mark - User Defaults
-
-- (void)registerDefaultPreferences
-{
-    NSDictionary *appDefaults = @{
-                                kDefaultXCCAutoOpenXcodeProject: @YES,
-                                kDefaultXCCLogLevel: [NSNumber numberWithInt:LOG_LEVEL_WARN],
-                                kDefaultXCCMaxNumberOfOperations: @20
-                                };
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    
-    [defaults registerDefaults:appDefaults];
-    [defaults synchronize];
-}
-
-#pragma mark - Logging methods
-
-- (void)initLogging
-{
-#if DEBUG
-    [DDLog addLogger:[DDTTYLogger sharedInstance]];
-    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
-    [DDLogLevel setLogLevel:LOG_LEVEL_VERBOSE];
-#else
-    [DDLog addLogger:[DDASLLogger sharedInstance]];
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    int logLevel = (int)[defaults integerForKey:kDefaultXCCLogLevel];
-    NSUInteger modifiers = [NSEvent modifierFlags];
-    
-    if (modifiers & NSAlternateKeyMask)
-        logLevel = LOG_LEVEL_VERBOSE;
-    
-    [DDLogLevel setLogLevel:logLevel];
-#endif
-}
-
 @end
